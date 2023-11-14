9835c0f41892f5816de47c81e692adc09e538255
RussellET <russ.trutane@gmail.com>
2023-11-14 15:07:26 -0800

    cleaned up my code so I repeat myself less, added text to clarify what each function does

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # this is my attempt at creating code that fulfills the assignment with some help from chatGPT
-# and the answer key. I should redo this and define more functions with individual purposes
-# instead of having a ton of stuff bundled into like 2 functions
-# also I should find a more eloquent way to create a dictionary instead of brute forcing it.
+# and the answer key. 
 def main():
     filename = "Frankenstein"
-    word_count = count_words(filename)
-    letter_count = create_letter_count(filename)
+    book = get_book(filename)
+    word_count = count_words(book)
+    letter_count = create_letter_count(book)
     letter_count_sorted = sort_dict(letter_count)
     print(f"--- Begin report of {filename} ---")
     print(f"{word_count} words found in the document")
@@ -16,51 +15,35 @@ def main():
 
     print("--- End report ---")
 
+# takes the filename, (which for this exercise is always Frankenstein) and returns a string
+# of the file's contents
+def get_book(filename):
+    with (open(f"books/{filename}.txt")) as f:
+        return f.read()
 
-def count_words(filename):
-     with open(f"books/{filename}.txt") as f:
-        file_contents = f.read()
-        split_book = file_contents.split()
-        num_words = len(split_book)
-        return num_words
-     
+# takes a string and returns how many words are in it
+def count_words(book):
+    split_book = book.split()
+    num_words = len(split_book)
+    return num_words
+
+# takes a dictionary of string : int and sorts it based on the value of the int descending
 def sort_dict(dictionary):
     return dict(sorted(dictionary.items(), key=lambda x: x[1], reverse = True))
 
-def create_letter_count(filename):
-    with open(f"books/{filename}.txt") as f:
-        file_contents = f.read()
-        file_lower = file_contents.lower()
-        letter_count = {'a': 0,
-                        'b': 0,
-                        'c': 0,
-                        'd': 0,
-                        'e': 0,
-                        'f': 0,
-                        'g': 0,
-                        'h': 0,
-                        'i': 0,
-                        'j': 0,
-                        'k': 0,
-                        'l': 0,
-                        'm': 0,
-                        'n': 0,
-                        'o': 0,
-                        'p': 0,
-                        'q': 0,
-                        'r': 0,
-                        's': 0,
-                        't': 0,
-                        'u': 0,
-                        'v': 0,
-                        'w': 0,
-                        'x': 0,
-                        'y': 0,
-                        'z': 0}
-        for letter in file_lower:
-            for key in letter_count:
-                if letter == key:
-                    letter_count[key] += 1
-        return letter_count
+# takes a string, makes it lower case, uses a dictionary of string : int to count how many
+# times a letter comes up
+def create_letter_count(book):
+    book_lower = book.lower()
+    letter_count = {}
+    junk = []
+    for letter in book_lower:
+        if not letter.isalpha():
+            pass
+        elif letter not in letter_count:
+            letter_count[letter] = 1
+        else: 
+            letter_count[letter] += 1
+    return letter_count
 
 main()
